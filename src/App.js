b589b2e20a044c226d7af05626710b365f1b2715
iqbalpa <iqbalpahlevi16@gmail.com>
2022-08-13 07:03:07 +0700

    created the header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Main from "./layout/Main/Main"
+import Header from "./layout/Header/Header"
 
 export default function App() {
   return (
     <>
-      <Main />
+      <Header />
+      {/* <Main /> */}
     </>
   )
 }
---
+++ src/layout/Header/Header.js
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+
+const Header = () => {
+    const [value, setValue] = useState("");
+
+    const handleChange = e => { setValue(e.target.value) };
+
+    return (
+        <div className="bg-gray-800 text-white h-14 flex flex-row justify-between items-center">
+            <p className="ml-4 text-xl font-bold">STUDIO GHIBLI</p>
+            <input value={value} onChange={handleChange} type="text" className="h-8 w-96 rounded-2xl p-2 text-black" />
+            <a href="https://github.com/iqbalpa" target="_blank" className="mr-4 text-lg font-semibold hover:text-gray-300 duration-75">IQBAL</a>
+        </div>
+    )
+}
+
+export default Header;
