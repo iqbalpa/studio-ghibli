83725df467792bd4092a9d9977d5280ad9fa0838
iqbalpa <iqbalpahlevi16@gmail.com>
2022-08-17 07:40:27 +0700

    created the film details page in FilmDetail.page

--- src/pages/FilmDetail/FilmDetail.js
+++ src/pages/FilmDetail/FilmDetail.js
@@ -1,10 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import API from "../../api/api";
+import { useParams } from "react-router";
 
 const FilmDetail = () => {
+    const [data, setData] = useState(null);
+    const { id } = useParams();
+
+    useEffect(() => {
+        const fetchFilm = async () => {
+            try {
+                const film = await API.fetchFilm(id);
+                setData(film);
+            } catch (error) {
+                console.log("error");
+            }
+        }
+
+        fetchFilm();
+    }, [data]);
+
     return (
-        <div>
-            <h1>test</h1>
-        </div>
+        <>
+            { data && 
+                <div>
+                    <img src={data.movie_banner} alt={data.title} />
+                    <p>{data.title}</p>   
+                    <p>{data.original_title}</p>  
+                    <p>{data.original_title_romanised}</p>  
+                    <p>{data.director}</p>  
+                    <p>{data.producer}</p>  
+                    <p>{data.release_date}</p>  
+                    <p>{data.running_time}</p>
+                    <p>{data.rt_score}</p>
+                    <p>{data.description}</p>  
+                </div>
+            }
+        </>
     )
 }
 
