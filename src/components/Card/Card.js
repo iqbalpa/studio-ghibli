6ff48d1e5a9c8a7f545e740af2381e35492de86f
iqbalpa <iqbalpahlevi16@gmail.com>
2022-08-17 07:08:11 +0700

    add the Link in Card.js for page routing

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,10 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-const Card = ({ image, title }) => {
+const Card = ({ id, image, title }) => {
     return (
-        <a href="#" className="w-[14rem] h-[21rem] m-5 flex items-center rounded-lg hover:scale-110 duration-100 hover:cursor-pointer">
-            <img src={image} alt={title} className="rounded-md" />
-        </a>
+        <Link to={`/film/${id}`} >
+            <div className="w-[14rem] h-[21rem] m-5 flex items-center rounded-lg hover:scale-110 duration-100 hover:cursor-pointer">
+                <img src={image} alt={title} className="rounded-md" />
+            </div>
+        </Link>
     )
 }
 
